80127104a65da9ac33278f165eee4f8bcc47d504
Josef Baumgartner <josef2801@gmail.com>
2023-12-03 09:16:51 +0100

    fixes

--- ports/rp2/boards/RPI_PICO/manifest.py
+++ ports/rp2/boards/RPI_PICO/manifest.py
@@ -2,16 +2,13 @@ include("$(PORT_DIR)/boards/manifest.py")
 
 require("bundle-networking")
 
-# mqtt_as
-freeze("$(MPY_DIR)/extmod/mqtt_as", "mqtt_as.py")
-
 # Bluetooth
-require("aioble")
+#require("aioble")
 require("ssd1306")
 require("dht")
 require("ds18x20")
 require("neopixel")
 require("onewire")
-require("umqtt.robust")
-require("umqtt.simple")
+#require("umqtt.robust")
+#require("umqtt.simple")
 require("upysh")
--- ports/rp2/boards/RPI_PICO/mpconfigboard.cmake
+++ ports/rp2/boards/RPI_PICO/mpconfigboard.cmake
@@ -1,2 +1,6 @@
 # cmake file for Raspberry Pi Pico
 set(PICO_BOARD "pico")
+
+# Board specific version of the frozen manifest
+set(MICROPY_FROZEN_MANIFEST ${MICROPY_BOARD_DIR}/manifest.py)
+
