ccae93f02f9ebf3db98de669917c11f26f5e831b
Josef Baumgartner <josef2801@gmail.com>
2024-12-25 20:33:46 +0100

    Add more modules

--- ports/esp32/boards/manifest.py
+++ ports/esp32/boards/manifest.py
@@ -1,15 +1,24 @@
 freeze("$(PORT_DIR)/modules")
 include("$(MPY_DIR)/extmod/asyncio")
+#include("$(MPY_DIR)/extmod/micropython-mqtt")
+
+#dd_library("micropython-mqtt", "$(MPY_DIR)/extmod/micropython-mqtt")
 
 # Useful networking-related packages.
 require("bundle-networking")
 
+#equire("micropython-mqtt", library="micropython-mqtt")
+package("mqtt_as", base_path="$(MPY_DIR)/extmod/micropython-mqtt")
+
+
 # Require some micropython-lib modules.
 require("aioespnow")
+require("aioble")
 require("dht")
 require("ds18x20")
 require("neopixel")
 require("onewire")
+require("ssd1306")
 require("umqtt.robust")
 require("umqtt.simple")
 require("upysh")
